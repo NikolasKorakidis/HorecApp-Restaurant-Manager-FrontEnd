f9457c1a5fcb57d1f8c0a7e361b0e809f9dc9d36
NikolasKorakidis <korakidisn@gmail.com>
2020-10-22 10:59:06 +0200

    Styled the Main Page

---
+++ src/components/TableCard.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { Order } from "../types/orderTypes";
+import { useDispatch } from "react-redux";
+import { createOrder } from "../store/orders/actions";
+
+const useStyles = makeStyles({
+  root: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
+
+interface Props {
+  tableId: number;
+  tableOrders: Order[];
+}
+
+export default function OutlinedCard(props: Props) {
+  const dispatch = useDispatch();
+  const { tableId, tableOrders } = props;
+  const classes = useStyles();
+  const bull = <span className={classes.bullet}>•</span>;
+
+  return (
+    <Card
+      style={
+        tableOrders.length > 0
+          ? { backgroundColor: "pink" }
+          : { backgroundColor: "lightgreen" }
+      }
+      className={classes.root}
+      variant="outlined"
+    >
+      <CardContent>
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          restaurapp
+        </Typography>
+        <Typography variant="h5" component="h2">
+          Table number: {tableId}
+        </Typography>
+        <Typography className={classes.pos} color="textSecondary">
+          {tableOrders.length > 0 ? "occupied" : "free"}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        {tableOrders.length > 0 ? (
+          <Button
+            onClick={() => (document.location.href = `/table/${tableId}`)}
+            style={{ backgroundColor: "lightgrey" }}
+            size="small"
+          >
+            Open Table
+          </Button>
+        ) : (
+          <Button
+            onClick={() => dispatch(createOrder(tableId))}
+            style={{ backgroundColor: "lightgrey" }}
+            size="small"
+          >
+            Create new Order
+          </Button>
+        )}
+      </CardActions>
+    </Card>
+  );
+}
