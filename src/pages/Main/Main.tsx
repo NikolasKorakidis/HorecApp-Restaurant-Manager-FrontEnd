cfd92902b463a85840b30e7dae80c3126dff8112
NikolasKorakidis <korakidisn@gmail.com>
2020-12-15 13:55:43 +0100

    Navigation changed to ReactLink

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { fetchOrders } from "../../store/orders/actions";
 import MainInterfaces from "../../components/NavigationBar/interfaces/MainInterfaces";
+import { Link } from "react-router-dom";
 import "./styles.css";
 
 export default function Main() {
@@ -15,32 +16,36 @@ export default function Main() {
     <div>
       <h1>RestaurApp</h1>
       <div className="buttons">
-        <MainInterfaces
-          image={
-            "https://www.zilliondesigns.com/blog/wp-content/uploads/restaurant-menus.jpg"
-          }
-          title={"restaurant"}
-          path={"restaurant"}
-          disc={"Restaurant Overview"}
-        />
-        <MainInterfaces
-          image={
-            "https://digitalrestro.files.wordpress.com/2014/07/restaurant-graphics.jpg"
-          }
-          title={"restaurant"}
-          path={"orders"}
-          disc={"Active Orders"}
-        />
-
-        <MainInterfaces
-          image={
-            "https://image.freepik.com/free-vector/elegant-restaurant-menu_23-2147492923.jpg"
-          }
-          title={"menu"}
-          path={"menu"}
-          disc={"Menu"}
-        />
-
+        <Link to="/restaurant" style={{ textDecoration: "none" }}>
+          <MainInterfaces
+            image={
+              "https://www.zilliondesigns.com/blog/wp-content/uploads/restaurant-menus.jpg"
+            }
+            title={"restaurant"}
+            path={"restaurant"}
+            disc={"Restaurant Overview"}
+          />
+        </Link>
+        <Link to="orders" style={{ textDecoration: "none" }}>
+          <MainInterfaces
+            image={
+              "https://digitalrestro.files.wordpress.com/2014/07/restaurant-graphics.jpg"
+            }
+            title={"restaurant"}
+            path={"orders"}
+            disc={"Active Orders"}
+          />
+        </Link>
+        <Link to="orders" style={{ textDecoration: "none" }}>
+          <MainInterfaces
+            image={
+              "https://image.freepik.com/free-vector/elegant-restaurant-menu_23-2147492923.jpg"
+            }
+            title={"menu"}
+            path={"menu"}
+            disc={"Menu"}
+          />
+        </Link>
         <MainInterfaces
           image={
             "https://cdn.thinglink.me/api/image/783748870432620544/1240/10/scaletowidth"
