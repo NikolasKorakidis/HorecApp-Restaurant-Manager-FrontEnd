d38698052b42aa14920155d6a1e86a5fb68727b9
nikolaskorakidis <korakidisn@gmail.com>
2021-01-02 14:37:28 +0100

    Structure on Managers View

--- src/pages/ManagersView/index.tsx
+++ src/pages/ManagersView/index.tsx
@@ -1,23 +1,74 @@
 import React from "react";
+import "./styles.css";
 
 export default function index() {
   return (
-    <div style={styles.main}>
+    <div className="main-manager-div">
       <div className="earnings">
-        <h1 className="header">manager's money per day</h1>
-        <p>Total: 300</p>
+        <h1 className="header">Daily Report</h1>
+        <p>Daily Total: 300 €</p>
       </div>
       <div className="orders">
-        <h1 className="header">manager's orders per day</h1>
-        <p>kajsdadhajshdjkahddjkasdh</p>
+        <h1 className="header">Daily Consumption</h1>
+        {data.map((item) => (
+          <p>
+            {" "}
+            {item.item} : {item.units}
+          </p>
+        ))}
+        <button
+          onClick={() => alert("Your Order has been Successfully placed")}
+        >
+          Make an Order
+        </button>
       </div>
     </div>
   );
 }
 
-const styles = {
-  main: {
-    // display: "flex",
-    margin: "auto",
+const data = [
+  {
+    item: "Coca Cola",
+    units: 15,
   },
-};
+  {
+    item: "Lagunitas IPA",
+    units: 75,
+  },
+  {
+    item: "Heineken",
+    units: 123,
+  },
+  {
+    item: "Fanta",
+    units: 45,
+  },
+  {
+    item: "Bacardi",
+    units: 5,
+  },
+  {
+    item: "Apple Pie",
+    units: 11,
+  },
+  {
+    item: "Margarita",
+    units: 1,
+  },
+  {
+    item: "Verdejo",
+    units: 115,
+  },
+  {
+    item: "Cabernet Sauvignon",
+    units: 42,
+  },
+  {
+    item: "Sprite",
+    units: 35,
+  },
+  {
+    item: "Haig",
+    units: 37,
+  },
+];
