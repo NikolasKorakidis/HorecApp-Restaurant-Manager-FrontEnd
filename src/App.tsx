8e0c79a94e920f1fb3175f6ba2699f8c793dfed8
nikolaskorakidis <korakidisn@gmail.com>
2021-01-05 17:34:47 +0100

    Initializing Intro for the POS

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import Main from "./pages/Main/Main";
@@ -7,14 +7,16 @@ import NavigationBar from "./components/NavigationBar//NavogationBar";
 import UpNavBar from "./components/NavigationBar/UpNavBar";
 import Table from "./pages/TableView/TableView";
 import Menu from "./pages/Menu/Menu";
-import ManagersView from "./pages/ManagersView/index"
+import ManagersView from "./pages/ManagersView/index";
 import ActiveOrders from "./pages/ActiveOrders/ActiveOrders";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { useSelector } from "react-redux";
 import { selectMode } from "./store/darkMode/selector";
+import Intro from "./pages/Intro/index";
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const mode = useSelector(selectMode);
 
   const theme = React.useMemo(
@@ -32,12 +34,12 @@ function App() {
         <CssBaseline />
         <UpNavBar />
         <Switch>
+          <Route strict path="/" component={loading ? Main : Intro} />
           <Route strict path="/manager" component={ManagersView} />
           <Route strict path="/orders" component={ActiveOrders} />
           <Route strict path="/menu" component={Menu} />
           <Route strict path="/table/:tableId" component={Table} />
           <Route strict path="/restaurant" component={RestaurantOverview} />
-          <Route strict path="/" component={Main} />
         </Switch>
         <NavigationBar />
       </ThemeProvider>
